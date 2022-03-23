bbd87521e8a2ec9e9510c506a518c80852d3f5b1
Daniel <taiwodaniel1998@gmail.com>
2022-03-23 18:49:30 +0100

    second project euler problem

--- 002.js
+++ 002.js
@@ -4,7 +4,17 @@ let a = 0, b = 1, c, sum = 0;
 
 // while the term in the fibonacci sequence is not greater than 4000000
 while (b <= 4000000) {
+    // summing the two previous values i.e a and b and generating new values after each iteration thereby generating the fibonacci term
     c = a + b;
     a = b;
     b = c;
+
+    // is the fibinacci term an even number
+    if (c % 2 == 0) {
+        sum += c;
+    }
 }
+
+// console.log(sum);
+
+// variable sum now hold the sum of the even terms in the fibonacci sequence whose values do not exceed 4000000 i.e 4613732
